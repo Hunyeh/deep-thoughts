acdd3aa94164297cedc5286c390b5c655a7155ab
Hunter <88465412+Hunyeh@users.noreply.github.com>
2022-01-12 21:37:38 -0500

    fixed error

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -26,6 +26,7 @@ module.exports = {
         try {
             // decode and attach user data to request object
             const {data} = jwt.verify(token, secret, {maxAge: expiration});
+            console.log(data)
             req.user = data;
         } catch {
             console.log('Invalid token');
