caf9113665823e54a0a6711ed94165f458d93e21
Hunter <88465412+Hunyeh@users.noreply.github.com>
2022-01-13 22:49:11 -0500

    added client

--- server/server.js
+++ server/server.js
@@ -1,31 +1,37 @@
 const express = require('express');
 const {ApolloServer} = require('apollo-server-express');
+const path = require('path');
 const {typeDefs, resolvers} = require('./schemas');
+const {authMiddleware} = require('./utils/auth');
 const db = require('./config/connection');
-const { authMiddleware } = require('./utils/auth');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 const startServer = async () => {
-  // create new apollo server and pass schema data
   const server = new ApolloServer({
     typeDefs,
     resolvers,
-    context: authMiddleware
+    context: authMiddleware,
   });
-  // starts apollo server
   await server.start();
-  // integrate our Apollo server with the Express application as middleware
-  server.applyMiddleware({app});
-  // log where we can go to test our GQL API
+  server.applyMiddleware({ app });
   console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 };
-startServer();
 
+startServer()
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
   app.listen(PORT, () => {
